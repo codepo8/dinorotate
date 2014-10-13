cfa30f02f917e7c8adad13607696f502f7f9a13f
Chris Heilmann <chris.heilmann@gmail.com>
2014-10-13 19:53:01 +0100

    rotate

---
+++ config.js
@@ -0,0 +1,214 @@
+var productRotationData_1412842036240 = {
+    "custom": {},
+    "settings": {
+        "control": {
+            "disableClick": false,
+            "disableMouseControl": false,
+            "maxZoom": 300,
+            "reversePan": false,
+            "mouseWheelZooms": false,
+            "enableSwing": true,
+            "reverseDrag": false,
+            "rotateZoom": false,
+            "screenshotFullImage": false,
+            "clickZoomsIn": false,
+            "arrowInterval": 100,
+            "zoomInStopsRotation": true,
+            "zoomOutStartsRotation": true,
+            "dragSpeed": 0.5,
+            "zoomSetsPan": false,
+            "disableDrag": false,
+            "playAfterDrag": false,
+            "zoomSteps": 2
+        },
+        "rotation": {
+            "rotatePeriod": 3,
+            "bounce": false,
+            "autoRestart": 0,
+            "disabledIfZoom": false,
+            "rotate": "true",
+            "rotateDirection": -1,
+            "rotationAxis": "vertical",
+            "firstImage": 0
+        },
+        "preloader": {
+            "showStartButton": false,
+            "showLoadedImages": false,
+            "image": "first",
+            "preLoadImages": false,
+            "type": "wave",
+            "color1": "#303030"
+        },
+        "userInterface": {
+            "showZoomButtons": true,
+            "showResetButton": "zoom",
+            "showZoombar": true,
+            "zoomBarClickable": true,
+            "defaultCursor": false,
+            "showToggleRotateButton": "zoom",
+            "showToggleFullscreenButton": true,
+            "showTogglePanButton": "zoom",
+            "showScreenshotButton": false,
+            "showTogglePlayButton": true,
+            "showArrows": true
+        }
+    },
+    "hotspots": null,
+    "images": [
+        {
+            "src": "images/Photo001.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo001.jpg"
+        },
+        {
+            "src": "images/Photo002.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo002.jpg"
+        },
+        {
+            "src": "images/Photo003.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo003.jpg"
+        },
+        {
+            "src": "images/Photo004.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo004.jpg"
+        },
+        {
+            "src": "images/Photo005.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo005.jpg"
+        },
+        {
+            "src": "images/Photo006.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo006.jpg"
+        },
+        {
+            "src": "images/Photo007.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo007.jpg"
+        },
+        {
+            "src": "images/Photo008.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo008.jpg"
+        },
+        {
+            "src": "images/Photo009.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo009.jpg"
+        },
+        {
+            "src": "images/Photo010.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo010.jpg"
+        },
+        {
+            "src": "images/Photo011.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo011.jpg"
+        },
+        {
+            "src": "images/Photo012.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo012.jpg"
+        },
+        {
+            "src": "images/Photo013.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo013.jpg"
+        },
+        {
+            "src": "images/Photo014.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo014.jpg"
+        },
+        {
+            "src": "images/Photo015.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo015.jpg"
+        },
+        {
+            "src": "images/Photo016.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo016.jpg"
+        },
+        {
+            "src": "images/Photo017.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo017.jpg"
+        },
+        {
+            "src": "images/Photo018.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo018.jpg"
+        },
+        {
+            "src": "images/Photo019.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo019.jpg"
+        },
+        {
+            "src": "images/Photo020.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo020.jpg"
+        },
+        {
+            "src": "images/Photo021.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo021.jpg"
+        },
+        {
+            "src": "images/Photo022.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo022.jpg"
+        },
+        {
+            "src": "images/Photo023.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo023.jpg"
+        },
+        {
+            "src": "images/Photo024.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo024.jpg"
+        },
+        {
+            "src": "images/Photo025.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo025.jpg"
+        },
+        {
+            "src": "images/Photo026.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo026.jpg"
+        },
+        {
+            "src": "images/Photo027.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo027.jpg"
+        },
+        {
+            "src": "images/Photo028.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo028.jpg"
+        },
+        {
+            "src": "images/Photo029.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo029.jpg"
+        },
+        {
+            "src": "images/Photo030.jpg",
+            "hotspots": {},
+            "srcLarge": "imageslarge/Photo030.jpg"
+        }
+    ]
+};
+
+//for maximum compatiblity, assign the data to 1. common named variable, 2. product - specific named variable
+
+var RotationData_dino90y4jb = productRotationData_1412842036240;
+var RotationData = productRotationData_1412842036240;
